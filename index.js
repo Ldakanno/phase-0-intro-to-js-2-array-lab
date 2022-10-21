4831c1fa8397e137af141c148764a18ca2f5c67e
Lianne Dakanno <liannedakanno@gmail.com>
2022-10-20 19:14:55 -0500

    commit tree fb1d49ce2201c4e79cd7d23b32a0861c95333818

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(-1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+}
+
+function appendCat(name) {
+    const appendCatArray = [...cats, name];
+    return appendCatArray;
+}
+
+function prependCat(name) {
+    const prependCatArray = [name, ...cats];
+    return prependCatArray;
+}
+
+function removeLastCat() {
+    const removeLastCatArray = [...cats];
+    removeLastCatArray.pop();
+    return removeLastCatArray;
+}
+
+function removeFirstCat() {
+    const removeFirstCatArray = [...cats];
+    removeFirstCatArray.shift();
+    return removeFirstCatArray;
+}
